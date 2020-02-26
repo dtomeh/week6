c575432fba5fe8fb32d7a7c033e7740d4d27d9ea
dtomeh <tomeh001@umn.edu>
2020-02-26 14:22:10 -0600

    Create citations_tbl

--- R/week6.R
+++ R/week6.R
@@ -8,3 +8,15 @@ citations <- stri_read_lines("../data/citations.txt")
 citations_txt <- citations[stri_isempty(citations)==FALSE]
 length(citations)-length(citations_txt)
 
+#Data Cleaning
+library(tidyverse)
+library(stringr)
+library(rebus)
+sample(citations_txt, 10)
+citations_tbl <- enframe(citations_txt, name=NULL, value="cite") %>% mutate(line=row_number()) %>%
+  mutate(cite= str_remove_all(cite,pattern="\"|\'")) %>%
+  mutate(year=str_extract(citations_tbl$cite, pattern = "\\d{4}")) %>%
+  mutate(page_start=str_match(cite, pattern="((\\d+)\\-(\\d+))")[,3]) %>%
+  mutate(perf_ref=str_detect(citations_tbl$cite, pattern = "performance")) %>%
+  mutate(title=str_match(citations_tbl$cite, pattern= "\\d{4}\\)\\.\\s?([:upper:][:graph:][^.(]+)")[,2]) %>%
+  mutate(first_author = str_match(cite, pattern="^\\*?\\s?([:upper:][:graph:]+\\,?\\s*[:upper:]\\.?\\s*?[:upper:]?\\.?\\s*?[:upper:]?\\.)")[,2])
