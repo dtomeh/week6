8ab3ce6abc2fce9d525f8104c566356af1c4d5df
dtomeh <tomeh001@umn.edu>
2020-02-26 14:17:20 -0600

    Data Import Complete

--- R/week6.R
+++ R/week6.R
@@ -1,3 +1,10 @@
 #R Studio API Code
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+
+#Data Import 
+library(stringi)
+citations <- stri_read_lines("../data/citations.txt")
+citations_txt <- citations[stri_isempty(citations)==FALSE]
+length(citations)-length(citations_txt)
+
